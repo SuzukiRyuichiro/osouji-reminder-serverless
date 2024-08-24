65bf2082067d1d0a0d15d4cdd3d04e0b288eead7
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-08-24 15:27:49 +0900

    feat: rent payment

---
+++ lambdas/monthly_rent_payment.py
@@ -0,0 +1,7 @@
+def run(event, context):
+    url = "https://notify-api.line.me/api/notify"
+    access_token = os.environ['LINE_NOTIFY_CLIENT_TOKEN']
+    headers = {'Authorization': 'Bearer ' + access_token}
+    message = '明日25日は家賃の支払日です。忘れないうちに家賃を払いましょう。'
+    payload = {'message': message }
+    r = requests.post(url, headers=headers, params=payload,)
